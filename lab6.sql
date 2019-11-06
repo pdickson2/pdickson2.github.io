cde3ee3754b3f451c0aa59571c6824893859ec40
pdickson2 <55104727+pdickson2@users.noreply.github.com>
2019-11-06 10:02:13 -0500

    Update lab6.sql

--- lab6.sql
+++ lab6.sql
@@ -1,11 +1,10 @@
---Select buildings from initial polygons
+--Select buildings from initial osm polygons to create a separate, buildings only analysis
 CREATE TABLE builds as
 SELECT way, building, osm_id
 FROM planet_osm_polygon
 WHERE building IS NOT null
 
---Intersect the buildings with the floodplain
---Start by creating a new table column to indicate if the building lies within the floodplain
+--Start by creating a new column in the building table to indicate if the building lies within the floodplain
 ALTER TABLE builds ADD COLUMN flooded Boolean
 
 --Update the buildings to indicate if they flooded based on intersection with water
@@ -18,7 +17,7 @@ WHERE st_intersects(way, (select geom FROM water))
 UPDATE builds 
 set way = st_centroid(way)
 
---Intersect the buildings with the subwards
+--Intersect the buildings with the subwards by creating a new column in the building table and then updating it accordingly with an intersection with the subwards
 ALTER TABLE builds ADD COLUMN subward integer
 
 UPDATE builds
@@ -27,6 +26,7 @@ FROM subwards
 WHERE st_intersects(builds.way, (subwards.geom))
 
 --Calculate percentage of flooded buildings by subward
+
 --Here, we created counts of the flooded buildings and total buildings within each subward
 Create table  joinsw as
 SELECT 
